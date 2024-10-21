f302664effea8675da128bfbfa7e9682a8ef334c
Tomasz Uszynski <tomaszuszynski3@gmail.com>
2024-10-22 00:55:01 +0100

    Renamed file and adde different regex.

--- Week04/findInFIie.py
+++ Week04/findInFIie.py
@@ -3,16 +3,19 @@
 
 import re
 
-regex = "\[.*\]"
+# Findind the date and time in the access log.
+# regex = "\[.*\]"    
+# Finding Web addresses in the access log.
+regex = "http[s]?://\\S+\\b"
+
 
 filename = "smallerAccess.log"
 
 with open(filename) as inputFile:
     for line in inputFile:
         foundTextList = re.findall(regex, line)
-        if (len(foundTextList)!= 0):
-            #print(foundTextList)
+        if len(foundTextList) != 0:
+            # print(foundTextList)
             foundText = foundTextList[0]
-            #print(foundText)
-            # if I did not want the [] at the beginning and end
-            print(foundText[1:-1])
+            # print(foundText)
+            print(foundText)
